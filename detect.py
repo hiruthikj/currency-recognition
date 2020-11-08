46b989b307a43da1f9d342af4634d94ca3f4fd1b
Hiruthik <hiruthik27@gmail.com>
2020-11-09 00:02:50 +0530

    50 rs recoginsed

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 from utils import *
 from matplotlib import pyplot as plt
 import os
+import cv2
 
 def main():
     print('Currency Recognition Program starting...')
@@ -12,11 +13,11 @@ def main():
     # Initiate ORB detector
     orb = cv2.ORB_create()
 
-    #test_img = read_img('files/test_100_2.jpg')
-    #test_img = read_img('files/test_50_2.jpg')
-    test_img = read_img('files/test_20_2.jpg')
-    #test_img = read_img('files/test_100_3.jpg')
-    #test_img = read_img('files/test_20_4.jpg')
+    BASE_DIR = 'files'
+    TEST_IMAGE_NAME = 'test_20_2.jpg'
+    TEST_IMAGE_LOC = os.path.join(BASE_DIR, TEST_IMAGE_NAME)
+
+    test_img = read_img(TEST_IMAGE_LOC)
 
     # resizing must be dynamic
     original = resize_img(test_img, 0.4)
@@ -53,58 +54,7 @@ def main():
 
     	print(i, ' ', training_set[i], ' ', len(good))
 
-    if max_val != 8:
-    	print(training_set[max_pt])
-    	print('good matches ', max_val)
-
-    max_val = 8
-    max_pt = -1
-    max_kp = 0
-
-    orb = cv2.ORB_create()
-    # orb is an alternative to SIFT
-
-    #test_img = read_img('files/test_100_2.jpg')
-    #test_img = read_img('files/test_50_2.jpg')
-    test_img = read_img('files/test_20_2.jpg')
-    #test_img = read_img('files/test_100_3.jpg')
-    #test_img = read_img('files/test_20_4.jpg')
-
-    # resizing must be dynamic
-    original = resize_img(test_img, 0.4)
-    display('original', original)
-
-    # keypoints and descriptors
-    (kp1, des1) = orb.detectAndCompute(test_img, None)
-
-    training_set = ['files/20.jpg', 'files/50.jpg', 'files/100.jpg', 'files/500.jpg']
-
-    for i in range(0, len(training_set)):
-    	# train image
-    	train_img = cv2.imread(training_set[i])
-
-    	(kp2, des2) = orb.detectAndCompute(train_img, None)
-
-    	# brute force matcher
-    	bf = cv2.BFMatcher()#cv2.NORM_HAMMING, crossCheck=True)
-        # returns k best matches
-    	all_matches = bf.knnMatch(des1, des2, k=2)
-
-    	good = []
-    	# give an arbitrary number -> 0.789
-    	# if good -> append to list of good matches
-    	for (m, n) in all_matches:
-    		if m.distance < 0.789 * n.distance:
-    			good.append([m])
-
-    	if len(good) > max_val:
-    		max_val = len(good)
-    		max_pt = i
-    		max_kp = kp2
-
-    	print(i, ' ', training_set[i], ' ', len(good))
-
-    if max_val != 8:
+    if max_val >= 8:
     	print(training_set[max_pt])
     	print('good matches ', max_val)
 
@@ -114,12 +64,6 @@ def main():
     	note = str(training_set[max_pt])[6:-4]
     	print('\nDetected denomination: Rs. ', note)
 
-    	# audio_file = 'audio/{}.mp3'.format(note)
-    	#audio_file = "value.mp3"
-    	#tts = gTTS(text=speech_out, lang="en")
-    	#tts.save(audio_file)
-    	#return_code = subprocess.call(["afplay", audio_file])
-    	#playsound(audio_file)
     	(plt.imshow(img3), plt.show())
 
     else:
@@ -130,12 +74,6 @@ def main():
     	note = str(training_set[max_pt])[6:-4]
     	print('\nDetected denomination: Rs. ', note)
 
-    	# audio_file = 'audio/{}.mp3'.format(note)
-    	#audio_file = "value.mp3"
-    	#tts = gTTS(text=speech_out, lang="en")
-    	#tts.save(audio_file)
-    	#return_code = subprocess.call(["afplay", audio_file])
-    	#playsound(audio_file)
     	(plt.imshow(img3), plt.show())
 
     print('Program exited')

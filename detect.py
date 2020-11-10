3d98079fbdc8f15077c778286ea44778c60aaa6a
Hiruthik <hiruthik27@gmail.com>
2020-11-10 15:07:44 +0530

    preprocess for train imgs

--- detect.py
+++ detect.py
@@ -8,20 +8,16 @@ import glob
 import os
 from pathlib import Path
 
-MIN_MATCH_COUNT = 10
-KERNEL_SIZE = 3
+MIN_MATCH_COUNT = 20
+KERNEL_SIZE = 13
+
+test_dir = 'testing_images'
+currency_dir = 'currency_images'
+test_image_name = 'test_50_2.jpg'
+# test_image_name = 'Cat03.jpg'
 
 def main():
-    print('Currency Recognition Program starting...')
-
-    max_matches = 0
-    best_i = -1
-    best_kp = 0
-    sum_kp = 0
-
-    test_dir = 'testing_images'
-    currency_dir = 'currency_images'
-    test_image_name = 'test_50_2.jpg'
+    print('Currency Recognition Program starting...\n')
 
     training_set = [
         img for img in glob.glob(os.path.join(currency_dir, "*.jpg"))
@@ -31,21 +27,10 @@ def main():
     ]
 
     test_image_loc = os.path.join(test_dir, test_image_name)
-    test_img = read_img(test_image_loc)
+    test_img = cv2.imread(test_image_loc)
 
-    # resizing to display
-    image_to_view = resize_img(test_img, 0.4)
-    display('INPUT', image_to_view)
-
-    test_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-    display('After Converting to grayscale', test_img)
-
-    test_img = cv2.equalizeHist(test_img.astype(np.uint8))
-    display('After Histogram Equalization', test_img)
-
-    # test_img = cv2.GaussianBlur(test_img, (KERNEL_SIZE,KERNEL_SIZE), sigmaX=0)
-    test_img = cv2.bilateralFilter(test_img, KERNEL_SIZE, KERNEL_SIZE*2, (KERNEL_SIZE+1)//2);
-    display('After bilateralBlur', test_img)
+    # preprocess image
+    test_img = preprocess(test_img)
 
     # Initiate ORB detector
     orb = cv2.ORB_create()
@@ -53,13 +38,16 @@ def main():
     # keypoints and descriptors
     kp1, des1 = orb.detectAndCompute(test_img, mask=None)
 
+    max_matches = -1
+    sum_kp = 0
+
     for i in range(len(training_set)):
-        train_img = cv2.imread(training_set[i],cv2.IMREAD_GRAYSCALE)
+        train_img = cv2.imread(training_set[i])#,cv2.IMREAD_GRAYSCALE)
+        train_img = preprocess(train_img, showImages=False)
         kp2, des2 = orb.detectAndCompute(train_img, mask = None)
 
         # brute force matcher
         bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
-
         # Match descriptors
         all_matches = bf.knnMatch(des1, des2, k=2)
 
@@ -67,7 +55,7 @@ def main():
 
         # store all the good matches as per Lowe's ratio test.
         for m, n in all_matches:
-            if m.distance < 0.75 * n.distance:
+            if m.distance < 0.7 * n.distance:
                 good.append([m])
 
         sum_kp += len(kp2)
@@ -77,32 +65,58 @@ def main():
             max_matches = num_matches
             best_i = i
             best_kp = kp2
+            best_img = train_img
 
-        print(i, ' ', training_set[i], ' ', len(good))
+        print(f'{i+1} \t {training_set[i]} \t {len(good)}')
 
     if max_matches >= MIN_MATCH_COUNT:
-        print(f'Good Match Found!\n{training_set_name[best_i]} has maximum matches of {max_matches}')
+        print(f'\nMatch Found!\n{training_set_name[best_i]} has maximum matches of {max_matches} ({len(best_kp)/sum_kp*100}%)')
 
-        train_img = cv2.imread(training_set[best_i])
-        match_img = cv2.drawMatchesKnn(test_img, kp1, train_img, best_kp, good, None)#, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        match_img = cv2.drawMatchesKnn(test_img, kp1, best_img, best_kp, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-        note = str(training_set_name[best_i])
+        note = training_set_name[best_i]
         print(f'\nDetected denomination: {note}')
 
-        plt.imshow(match_img), plt.title('DETECTED MATCH'), plt.show()
+        plt.imshow(match_img), plt.title(f'DETECTED MATCH: {note}'), plt.show()
 
     else:
-        print('No Good Matches, closest one has {max_matches} matches')
-        train_img = cv2.imread(training_set[best_i])
-        closest_match = cv2.drawMatchesKnn(test_img, kp1, train_img, best_kp, good, 4)
+        print(f'\nNo Good Matches, closest one has {max_matches} matches')
 
-        note = str(training_set_name[best_i])
-        print(f'\nPredicted denomination: {note}')
+        closest_match = cv2.drawMatchesKnn(test_img, kp1, best_img, best_kp, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+        note = training_set_name[best_i]
+        # print(f'\nPredicted denomination: {note}')
+
+        plt.imshow(closest_match), plt.title('NO MATCH'), plt.show()
+
+    print('\nProgram exited')
 
 
-        plt.imshow(closest_match), plt.title('CLOSEST MATCH'), plt.show()
 
-    print('Program exited')
+
+
+
+def preprocess(img, showImages = True):
+    #showing the image inputed
+    img = resize_img(img, 0.4)
+    if showImages:
+        display('INPUT AFTER RESIZE', img)
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if showImages:
+        display('After Ctest_50_2onverting to Grayscale', img)
+
+    img = cv2.equalizeHist(img.astype(np.uint8))
+    if showImages:
+        display('After Histogram Equalization', img)
+
+    # test_img = cv2.GaussianBlur(test_img, (KERNEL_SIZE,KERNEL_SIZE), sigmaX=0)
+    img = cv2.bilateralFilter(img, KERNEL_SIZE, KERNEL_SIZE*2, KERNEL_SIZE//2)
+    if showImages:
+        display('After Bilateral Blur', img)
+
+    return img
+
 
 if __name__ == '__main__':
     main()
